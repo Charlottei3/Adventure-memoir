09e10c68461040a57b3e5a612766a3da61abe2fa
Ghost3i <validdraig1st@gmail.com>
2024-04-17 02:13:52 +0700

    Enemy boss v1

---
+++ Assets/Game/Scripts/Enemy/MeleeEnemyBoss.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyBoss : MonoBehaviour
+{
+    [SerializeField] private float _attackCountDown, _distance, _radius;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _timeCountDown = Mathf.Infinity;
+    [SerializeField] private CircleCollider2D _circleCollider;
+    [SerializeField] private LayerMask _layerPlayer;
+    [SerializeField] private Animator _anim;
+
+    private void Update()
+    {
+        _timeCountDown += Time.deltaTime;
+
+        if (PlayerIntheRegion())
+        {
+            if (_timeCountDown >= _attackCountDown)
+            {
+                _timeCountDown = 0;
+                _anim.SetTrigger("Attack");
+            }
+        }
+    }
+
+    private bool PlayerIntheRegion()
+    {
+        RaycastHit2D region = Physics2D.CircleCast(_circleCollider.bounds.center + transform.right * _distance * transform.localScale.x,
+            _radius, Vector2.left, 0, _layerPlayer);
+
+        return region.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(_circleCollider.bounds.center + transform.right * _distance * transform.localScale.x, _radius);
+    }
+}
